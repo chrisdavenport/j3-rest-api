4bb4b25be0b1499c8ad5dd315fc89e06094d621a
Chris Davenport <chris.davenport@joomla.org>
2013-03-16 11:29:05 +0000

    Added Mike Kelly's HAL Browser. Added option to replace relative links with absolute links so the HAL Browser works.

---
+++ application/hal.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * Class to represent a HAL standard object.
+ *
+ */
+class ApiApplicationHal
+{
+	/*
+	 * Array of properties of the HAL object.
+	 */
+	protected $properties = array();
+
+	/**
+	 * Add link object.
+	 * Link objects with the same rel name are contained in an array.
+	 *
+	 * @param  HalLink $link  Link object.
+	 *
+	 * @return Hal This method may be chained.
+	 */
+	public function addLink(ApiApplicationHalLink $link)
+	{
+		// If there is no _links property, create it.
+		if (!isset($this->properties['_links']))
+		{
+			$this->properties['_links'] = new stdClass;
+		}
+
+		// Get the link relation name.
+		$rel = $link->getRel();
+
+		// Add the link to the array of links for the given link relation name.
+		if (isset($this->properties['_links']->$rel))
+		{
+			$this->properties['_links']->$rel = array_merge($this->properties['_links']->$rel, array($link));
+		}
+		else
+		{
+			$this->properties['_links']->$rel = array($link);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Embed objects.
+	 *
+	 * @param  string  $name  Name (rel) of embedded objects.
+	 * @param  array   $data  Array of objects to be embedded.
+	 */
+	public function embed($name, $data)
+	{
+		// If there is no _embedded property, create it.
+		if (!isset($this->properties['_embedded']))
+		{
+			$this->properties['_embedded'] = new stdClass;
+		}
+
+		$this->properties['_embedded']->$name = $data;
+
+		return $this;
+	}
+
+	/**
+	 * Method to get the value of a property.
+	 *
+	 * @param  string  $name     Name of the property.
+	 * @param  mixed   $default  Value returned if property does not exist.
+	 *
+	 * @return mixed Value of the property.
+	 */
+	public function get($name, $default = null)
+	{
+		return isset($this->properties[$name]) ? $this->properties[$name] : $default;
+	}
+
+	/**
+	 * Method to return an object suitable for serialisation.
+	 *
+	 * @return stdClass A HAL object suitable for serialisation.
+	 */
+	public function getHal()
+	{
+		$hal = new stdClass;
+
+		// Links with the same link relation name are stored in an array, but if
+		// the array contains only one item, then we drop the array.
+		if (isset($this->properties['_links']) && !empty($this->properties['_links']))
+		{
+			// Get the links object.
+			$links = $this->properties['_links'];
+
+			// Convert single entry arrays to direct links.
+			foreach ($links as $rel => $link)
+			{
+				if (is_array($link))
+				{
+					if (count($link) == 1)
+					{
+						$links->$rel = $link[0];
+					}
+					else
+					{
+						$links->$rel = $this->links[$rel];
+					}
+				}
+			}
+
+			// Replace the links object.
+			$this->properties['_links'] = $links;
+		}
+
+		// Copy all the properties into the HAL object.
+		if (!empty($this->properties))
+		{
+			foreach ($this->properties as $key => $value)
+			{
+				$hal->$key = $value;
+			}
+		}
+
+		return $hal;
+	}
+
+	/**
+	 * Method to load an object or an array into this HAL object.
+	 *
+	 * @param  object  $object  Object whose properties are to be loaded.
+	 *
+	 * @return object This object for chaining.
+	 */
+	public function load($object)
+	{
+		foreach ($object as $name => $value)
+		{
+			$this->properties[$name] = $value;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Method to set the value of a property.
+	 *
+	 * @param  string  $name  Name of the property.
+	 * @param  mixed   $value Value of the property.
+	 *
+	 * @return object  This method may be chained.
+	 */
+	public function set($name, $value)
+	{
+		$this->properties[$name] = $value;
+
+		return $this;
+	}
+
+}
---
+++ document/hal/json.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * @package     Joomla.Services
+ * @subpackage  Document
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * ApiDocumentHal class, provides an easy interface to parse and display HAL+JSON output
+ *
+ * @package     Joomla.Services
+ * @subpackage  Document
+ * @see         http://stateless.co/hal_specification.html
+ * @since       3.2
+ */
+class ApiDocumentHalJson extends JDocument
+{
+	/**
+	 * Document name
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	protected $_name = 'joomla';
+
+	/**
+	 * Render hrefs as absolute or relative?
+	 */
+	protected $absoluteHrefs = false;
+
+	/**
+	 * Class constructor
+	 *
+	 * @param   array  $options  Associative array of options
+	 *
+	 * @since  3.2
+	 */
+	public function __construct($options = array())
+	{
+		parent::__construct($options);
+
+		// Set default mime type.
+		$this->_mime = 'application/json';
+
+		// Set document type.
+		$this->_type = 'hal+json';
+
+		// Set absolute/relative hrefs.
+		$this->absoluteHrefs = isset($options['absoluteHrefs']) ? $options['absoluteHrefs'] : false;
+	}
+
+	/**
+	 * Render the document.
+	 *
+	 * @param   boolean  $cache   If true, cache the output
+	 * @param   array    $params  Associative array of attributes
+	 *
+	 * @return  The rendered data
+	 *
+	 * @since  3.2
+	 */
+	public function render($cache = false, $params = array())
+	{
+		JResponse::allowCache($cache);
+		JResponse::setHeader('Content-disposition', 'attachment; filename="' . $this->getName() . '.json"', true);
+
+		// Unfortunately, the exact syntax of the Content-Type header
+		// is not defined, so we have to try to be a bit clever here.
+		$contentType = $this->_mime;
+		if (stripos($contentType, 'json') === false)
+		{
+			$contentType .= '+' . $this->_type;
+		}
+		$this->_mime = $contentType;
+
+		parent::render();
+
+		// Get the HAL object from the buffer.
+		$hal = $this->getBuffer();
+
+		// If required, change relative links to absolute.
+		if ($this->absoluteHrefs)
+		{
+			// Adjust hrefs in the _links object.
+			$this->relToAbs($hal->_links);
+
+			// Adjust hrefs in the _embedded object (if there is one).
+			if (isset($hal->_embedded))
+			{
+				foreach ($hal->_embedded as $rel => $resources)
+				{
+					foreach ($resources as $id => $resource)
+					{
+						if (isset($resource->_links))
+						{
+							$this->relToAbs($resource->_links);
+						}
+					}
+				}
+			}
+		}
+
+		// Return it as a JSON string.
+		return json_encode($hal);
+	}
+
+	/**
+	 * Returns the document name
+	 *
+	 * @return  string
+	 *
+	 * @since  3.2
+	 */
+	public function getName()
+	{
+		return $this->_name;
+	}
+
+	/**
+	 * Method to convert relative to absolute links.
+	 *
+	 * @param  object $links  Links object (eg. _links).
+	 */
+	protected function relToAbs($links)
+	{
+		// Adjust hrefs in the _links object.
+		foreach ($links as $rel => $link)
+		{
+			if (substr($link->href, 0, 1) == '/')
+			{
+				$links->$rel->href = rtrim(JUri::base(), '/') . $link->href;
+			}
+		}
+	}
+
+	/**
+	 * Sets the document name
+	 *
+	 * @param   string  $name  Document name
+	 *
+	 * @return  JDocumentJSON instance of $this to allow chaining
+	 *
+	 * @since   3.2
+	 */
+	public function setName($name = 'joomla')
+	{
+		$this->_name = $name;
+
+		return $this;
+	}
+}
